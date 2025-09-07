acaa30f804b0bc02f32d7e5e824e212146c651e6
kormuyang <phantakan04@gmail.com>
2025-09-07 14:33:42 +0700

    add files into intro-data-struct/grader

--- intro-data-struct/grader/d64_q3a_queue_check.cpp
+++ intro-data-struct/grader/d64_q3a_queue_check.cpp
@@ -21,6 +21,8 @@ pair<bool, int> check(int front, int size, int cap, int last, int correction) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     int q;
     cin >> q;
     while (q--) {
