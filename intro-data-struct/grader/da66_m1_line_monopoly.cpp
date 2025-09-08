36af6481dab770a43437344dc1ef517c60307249
kormuyang <phantakan04@gmail.com>
2025-09-08 18:17:58 +0700

    add files into intro-data-struct/grader

--- intro-data-struct/grader/da66_m1_line_monopoly.cpp
+++ intro-data-struct/grader/da66_m1_line_monopoly.cpp
@@ -5,13 +5,29 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    set<pair<int, int>> s, reversed_s;
+    set<pair<int, int>> s;
     while (n--) {
-        int q;
-        cin >> q;
-        if (q == 1) {
-            int a, b;
-            cin >> a >> b;
+        int op;
+        cin >> op;
+        if (op == 1) {
+            int l, r;
+            cin >> l >> r;
+            auto it = s.upper_bound({l, -1});
+            if (it != s.begin()) {
+                --it;
+                if (it->first <= l && l <= it->second) {
+                    l = min(l, it->first);
+                    r = max(r, it->second);
+                    it = s.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+            while (it != s.end() && r >= it->first) {
+                r = max(r, it->second);
+                it = s.erase(it);
+            }
+            s.insert({l, r});
         } else {
             cout << s.size() << '\n';
         }
